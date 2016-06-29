c710da3182a95db716b069cefd43c1e0c34077d5
zhoujian1982318 <jian.zhou@relay2.com>
2016-06-30 07:09:13 +0800

    add  Annotation "Response Body"  on MonitorController
    fix the bug reqest "/monitor/clients/greeting"

--- study-spring-mvc/src/main/java/hello/MonitorController.java
+++ study-spring-mvc/src/main/java/hello/MonitorController.java
@@ -3,13 +3,14 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/monitor/clients")
 public class MonitorController {
 	
 	@RequestMapping(value="/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String theName) {
+    public @ResponseBody Greeting greeting(@RequestParam(value="name", defaultValue="World") String theName) {
 		String name = theName;
 		Greeting greeting = new Greeting();
         return greeting;
